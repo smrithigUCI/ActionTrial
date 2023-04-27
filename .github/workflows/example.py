7dce3f3cef6714a49b62d660bf987dee256e1594
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-04-26 23:32:35 -0700

    figuring out the date
    figuring out the date

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -6,9 +6,13 @@ def main():
         contents = str(contents);
         print('reading file content');
         print('contents->',contents);
+        c1=date.today()
+        print("date.today()->",c1);
+        c1=c1+'\n';
+        print("c1-now->",c1);
         if contents == '':
           print("found file empty");
-        if (date.today()==contents):
+        if (date.today()==c1):
           print("today's date is->",contents);
 
 if __name__=='__main__':
