b2ab8b23ca100f25b211161c73e5c47c553ed9b1
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-04-26 17:40:26 -0700

    adding the reading of outputFile.txt
    Adding the angle outputFile.txt

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,5 +1,10 @@
 def main():
   print("Hello from GitHub");
+  with open('outputFile.txt') as f:
+        contents = f.readlines();
+        contents = str(contents);
+        if contents == '':
+          print("insude the text file");
 
 if __name__=='__main__':
   main()
