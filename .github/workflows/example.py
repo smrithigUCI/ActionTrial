d764c9b45d40299a7d5def96df635d1cafde579d
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-04-27 00:37:11 -0700

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,7 +1,7 @@
 from datetime import date
 def main():
   print("Hello from GitHub");
-  with open('.github/workflows/outputFile1.txt') as f:
+  with open('.github/workflows/outputFile1.txt','r+') as f:
         contents = f.readlines()
         contents = contents.pop();
         contents = contents.rstrip();
@@ -9,7 +9,6 @@ def main():
         print('contents->',contents);
         print('date.today()->',date.today());
         if (str(date.today())==contents):
-          print("today's date is->",contents);
-          f.write(" SG awesome as always")
+          print("today's date is-> SG is awesome->",contents);
 if __name__=='__main__':
   main()
