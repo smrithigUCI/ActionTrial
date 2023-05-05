ff845d51f74585a93639ade755898b4c9f8f2397
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 13:26:25 -0700

    Update fileManipulation.py

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -1,2 +1,18 @@
-with open('.github/workflows/outputFile1.txt', 'w') as f:
-    f.write('Create a new text file!')
+import json
+import requests
+def UploadToDrive(".github/workflows/outputFile1.txt","outputFile1.txt"):
+
+    headers = {"Authorization":"Bearer ya29.a0Ael9sCO2lIUKj9IjCAF9pn1Jh4ibbE9raibgODyeQ6qdMIpc_TCm5tzR2a8g5mg_qqf3y_EcKJMBI2RLGaXAiFGp8gWHYVDsiYY--kPtAuZzc0EwYpoRwGrWxXs33lGUZySYXQnkepoK2OSynPRwVN3GP3AqaCgYKAcESARMSFQF4udJhyn8VBTqoAMQRkXCI9HzufQ0163"}
+
+    para = {
+        "name":filename,
+        "parents":["1DBr5USVxg6LJw_HDiBK743vjIFo4gIRK"]
+    }
+    files = {
+        'data':('metadata',json.dumps(para),'application/json;charset=UTF-8'),
+        'file':open(filepath,'rb')
+    }
+
+    r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",headers=headers,files=files)
+    print(f"Uploaded {filename} to Drive")
+UploadToDrive(".github/workflows/outputFile1.txt","outputFile1.txt")
