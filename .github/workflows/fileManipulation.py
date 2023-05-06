fff0be7b1588254d21605f16a0a3c313dbcf7059
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 17:58:48 -0700

    Update fileManipulation.py

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file('.github/workflows/advantechuci-1fdd9c7f63c3.json')
 service = build('drive', 'v3', credentials=credentials)
 # Find the file by its name or other identifier
-file_name = 'outputFile1.docx'
+file_name = 'outputFile1'
 response = service.files().list(q=f"name='{file_name}'").execute()
 print('response->',response)
 file_id = response['files'][0]
