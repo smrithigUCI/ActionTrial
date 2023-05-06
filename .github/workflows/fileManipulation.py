0c1e3b25fd61188d9d36e1f1a507bcaba6171a16
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-06 12:43:57 -0700

    importin IO

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -1,7 +1,10 @@
-from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import io
+
 credentials = service_account.Credentials.from_service_account_file('.github/workflows/advantechuci-1fdd9c7f63c3.json')
-service = build('drive', 'v3', credentials=credentials)
+drive_service = build('drive', 'v3', credentials=credentials)
+
 # Find the file by its name or other identifier
 file_content = 'Hello, World!'
 file_name = 'example.txt'
@@ -10,9 +13,18 @@ metadata = {
     'mimeType': 'text/plain'
 }
 
-media = service.files().create(
-    body=metadata
-    #media_body=io.BytesIO(file_content.encode())
+file_metadata = {
+    'name': 'example.txt',  # Name of the file
+    'mimeType': 'text/plain'  # MIME type of the file
+}
+
+file_content = 'This is the content of the text file.'
+
+file = drive_service.files().create(
+    body=file_metadata,
+    media_body=io.BytesIO(file_content.encode('utf-8'))
 ).execute()
+
+print('File created:', file.get('id'))
 
 
