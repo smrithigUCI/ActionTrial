9ca5e56e44fbbee4910a3d5fc95e484d73fbee46
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 17:56:41 -0700

    Update fileManipulation.py

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -6,10 +6,10 @@ service = build('drive', 'v3', credentials=credentials)
 file_name = 'outputFile1.docx'
 response = service.files().list(q=f"name='{file_name}'").execute()
 print('response->',response)
-"""file_id = response['files'][0]['id']
-print('file_id->',file_id)"""
+file_id = response['files'][0]
+print('file_id->',file_id)
 content = 'Hello, World!'
-media_body = service.files().get_media('1sTNwhKyUm5WVWJLD1nxpeJr17xe7r4xo9WJhDmdsrek').execute()
+media_body = service.files().get_media(file_id).execute()
 print('media_body->',media_body)
 media_body += content.encode()
 updated_file = service.files().update(
