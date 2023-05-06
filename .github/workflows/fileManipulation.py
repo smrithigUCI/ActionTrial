a3ed65b1cbae9ab825784c495082c4cd2a86efd9
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 18:34:31 -0700

    creating a txt file
    creating a txt file

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -3,8 +3,16 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file('.github/workflows/advantechuci-1fdd9c7f63c3.json')
 service = build('drive', 'v3', credentials=credentials)
 # Find the file by its name or other identifier
-folder_id = '1oMYGCzitX98Oldeu5MDmhBerTko2WcKe'
-file_name='myDate1.txt'
-response = service.files().list(q=f"name='{file_name}' and '{folder_id}' in parents").execute()
-print(response)
+file_content = 'Hello, World!'
+file_name = 'example.txt'
+metadata = {
+    'name': file_name,
+    'mimeType': 'text/plain'
+}
+
+media = service.files().create(
+    body=metadata,
+    media_body=io.BytesIO(file_content.encode())
+).execute()
+
 
