1e0fa00915e9e2e37493eac79498364b54cd2253
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 18:22:28 -0700

    Update fileManipulation.py

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -3,8 +3,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file('.github/workflows/advantechuci-1fdd9c7f63c3.json')
 service = build('drive', 'v3', credentials=credentials)
 # Find the file by its name or other identifier
-response = service.files().list().execute()
-files = response.get('files', [])
-print(files)
-for file in files:
-    print(f"File Name: {file['name']}, File ID: {file['id']}")
+folder_id = '1oMYGCzitX98Oldeu5MDmhBerTko2WcKe'
+response = service.files().list(q=f"name='{file_name}' and '{folder_id}' in parents").execute()
+print(response)
+
