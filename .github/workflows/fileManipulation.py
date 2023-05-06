6db2b707b2cdade8975dac575edb0ba4011c54fc
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 17:51:45 -0700

    changing key location

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -1,6 +1,6 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-credentials = service_account.Credentials.from_service_account_file('.github/workflows/client_secret_217221968848-frebl3d7fmf13o36hq5fjv0ar5c88091.apps.googleusercontent.com.json')
+credentials = service_account.Credentials.from_service_account_file('.github/workflows/advantechuci-1fdd9c7f63c3.json')
 service = build('drive', 'v3', credentials=credentials)
 # Find the file by its name or other identifier
 file_name = 'outputFile1.docx'
