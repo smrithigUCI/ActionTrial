f3b80bc782cad41ee6f96c07455181fc8f132d24
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-05 23:42:32 -0700

    Update GUINotification.py

--- GUINotification.py
+++ GUINotification.py
@@ -11,7 +11,7 @@ Commit remark : Borders for image done
 #GUI required import
 #from tkinter import *
 #import tkinter as tk
-import tkinter
+import tkinter as tk
 import numpy as np
 #import tkinter.font as font
 import customtkinter
