1b32cc25a501c7f04b9f07f60438fd40e0353afc
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-11 15:01:59 -0700

    Update fileManipulation.py

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -14,7 +14,7 @@ blob_list =[]
 print('Hi')
 sas_i=generate_blob_sas(account_name=account_name,container_name=container_name,blob_name='myDate.txt',account_key=account_key,permissions=BlobSasPermissions(read=True),expiry=datetime.utcnow()+timedelta(hours=1))
 print(sas_i)
-df=pd.read_csv("https://sunflowerweedimage.blob.core.windows.net/sunfloweweedimagecontainer/myDate2.txt?"+sas_i")
+df=pd.read_csv("https://sunflowerweedimage.blob.core.windows.net/sunfloweweedimagecontainer/myDate.txt?"+sas_i")
 try:
     df[date.today()];
     print('all good')
