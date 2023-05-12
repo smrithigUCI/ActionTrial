210881dd68a8758b2378e83b487644616c274e25
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-11 18:59:57 -0700

    https to http
    https to http

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -15,14 +15,14 @@ def startPump():
     headers = {"Content-Type": "application/json","Authorization": "Basic cm9vdDowMDAwMDAwMA==","Cookie": "Cookie=adamsessionid=12965427BA2"}
     Flag = True;
     while(Flag):
-        response = requests.request("POST", "https://169.234.25.18/do_value/slot_0/ch_0", json=payload, headers=headers)
+        response = requests.request("POST", "http://169.234.25.18/do_value/slot_0/ch_0", json=payload, headers=headers)
         time.sleep(58)
         print(response)
         Flag = False;
                 
             #switching the relay 0 OFF
     payload1 = {"Ch": 0,"Md": 0,"Val": 0,"Stat":0 ,"PsCtn": 1,"PsStop": 0,"PsIV": 0}
-    response1 = requests.request("POST", "https://169.234.25.18/do_value/slot_0/ch_0", json=payload1, headers=headers)
+    response1 = requests.request("POST", "http://169.234.25.18/do_value/slot_0/ch_0", json=payload1, headers=headers)
             
     if response1.status_code == 200:
         msg = "The first preventive herbicide spray is done kindly place the robot on the field to monitor the further emergence of weed"
