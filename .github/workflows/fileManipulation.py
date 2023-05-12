ac52cb1e7839fda7d328989b5c214fc61328c27a
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2023-05-11 18:55:40 -0700

    val:0
    val:0

--- .github/workflows/fileManipulation.py
+++ .github/workflows/fileManipulation.py
@@ -21,7 +21,7 @@ def startPump():
         Flag = False;
                 
             #switching the relay 0 OFF
-    payload1 = {"Ch": 0,"Md": 0,"Val": 1,"Stat":0 ,"PsCtn": 1,"PsStop": 0,"PsIV": 0}
+    payload1 = {"Ch": 0,"Md": 0,"Val": 0,"Stat":0 ,"PsCtn": 1,"PsStop": 0,"PsIV": 0}
     response1 = requests.request("POST", "https://169.234.25.18/do_value/slot_0/ch_0", json=payload1, headers=headers)
             
     if response1.status_code == 200:
